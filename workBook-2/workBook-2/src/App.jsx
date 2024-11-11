5e5592befda23c9c1dd341eb3251624eb99a06e0
sungyeon <tjddus0731@naver.com>
2024-11-12 01:17:55 +0900

    TodoList 리팩토링

--- workBook-2/workBook-2/src/App.jsx
+++ workBook-2/workBook-2/src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useContext } from 'react';
 import './App.css';
 import styles from './todoList.module.css';
+import { TodoContext } from './context/TodoContext';
 
 // Input 컴포넌트
 function Input({ value, onChange }) {
@@ -24,43 +25,22 @@ function Button({ onClick, children, type = "button" }) {
 }
 
 function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, task: '투두 만들어보기' },
-    { id: 2, task: '박성연' },
-  ]);
-
-  const [text, setText] = useState('');
-  const [editingId, setEditingId] = useState('');
-  const [editText, setEditText] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-
-  // 1. 추가하기
-  const addTodo = () => {
-    setTodos((prev) => [
-      ...prev,
-      { id: Math.floor(Math.random() * 100) + 2, task: text },
-    ]);
-    setText('');
-  };
-
-  // 2. 삭제하기
-  const deleteTodo = (id) => {
-    setTodos((prev) => prev.filter((item) => item.id !== id));
-  };
-
-  // 3. 수정하기
-  const updateTodo = (id, text) => {
-    setTodos((prev) =>
-      prev.map((item) => (item.id === id ? { ...item, task: text } : item))
-    );
-    setEditingId('');
-  };
+  const {
+    todos,
+    text,
+    setText,
+    editingId,
+    setEditingId,
+    editText,
+    setEditText,
+    handleSubmit,
+    addTodo,
+    deleteTodo,
+    updateTodo,
+  } = useContext(TodoContext);
 
   return (
-      <div className={styles.container}>
+    <div className={styles.container}>
       <form onSubmit={handleSubmit}>
         <Input value={text} onChange={setText} />
         <Button onClick={addTodo} type="submit">
